a7bf3adf367962a5edf43647adadb9ae0b1d216a
Nazar Kislichenko <nazar.kislichenko@gmail.com>
2021-10-12 15:32:45 +0300

    Change name app

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'smartmenu-app '),
+    'name' => env('APP_NAME', 'Smartmenu'),
 
     /*
     |--------------------------------------------------------------------------
