c3b4cead917496b74621d2ad2dd75bf8933d0908
Nazar Kislichenko <nazar.kislichenko@gmail.com>
2023-06-25 00:44:33 +0300

    Updated laravel to v10 and installed Breeze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,189 +1,36 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>{{ config('app.name') }}</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/dragula-js/dragula.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/adminlte.css') }}" rel="stylesheet">
-    <link href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}" rel="stylesheet">
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-    <div id="app" class="wrapper">
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-            </ul>
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Messages Dropdown Menu -->
-                <!-- Notifications Dropdown Menu -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">15</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-item dropdown-header">15 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-users mr-2"></i> 8 friend requests
-                            <span class="float-right text-muted text-sm">12 hours</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-file mr-2"></i> 3 new reports
-                            <span class="float-right text-muted text-sm">2 days</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
                     </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{asset('storage/images/no-image.png')}}" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
-                    </div>
-                </div>
+                </header>
+            @endif
 
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <!-- Add icons to the links using the .nav-icon class
-                             with font-awesome or any other icon font library -->
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">
-                                <i class="nav-icon fas fa-home"></i>
-                                <p>
-                                    Home
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/dishes" class="nav-link">
-                                <i class="nav-icon fas fa-drumstick-bite"></i>
-                                <p>
-                                    Dishes
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/menus" class="nav-link">
-                                <i class="nav-icon fas fa-utensils"></i>
-                                <p>
-                                    Menu
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
-        </aside>
-        <div class="content-wrapper">
-            <section class="content">
-                @yield('content')
-            </section>
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
-    </div>
-</body>
-<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-<script src="{{asset('plugins/dragula-js/dragula.min.js')}}"></script>
-<script src="{{asset('js/adminlte.js')}}"></script>
-<script>
-    dragula([document.querySelector('.dishesDrag'), document.querySelector('.dishesDrop')],{
-
-        // or 'horizontal'
-        direction: 'vertical',
-
-        // determine whether to copy elements rather than moving
-        // if a method is passed, it'll be called whenever an element starts being dragged in order to decide whether it should follow copy behavior or not.
-        copy: false,
-
-        // determine whether to sort elements in copy-source containers.
-        copySortSource: false,
-
-        // spilling will put the element back where it was dragged from, if this is true
-        revertOnSpill: false,
-
-        // spilling will `.remove` the element, if this is true
-        removeOnSpill: false,
-
-        // set the element that gets mirror elements appended
-        mirrorContainer: document.body,
-
-        // allows users to select input text
-        ignoreInputTextSelection: true,
-
-        // allows users to select the amount of movement on the X axis before it is considered a drag instead of a click
-        slideFactorX: 0,
-
-        // allows users to select the amount of movement on the Y axis before it is considered a drag instead of a click
-        slideFactorY: 0,
-
-        // only elements in drake.containers will be taken into account
-        isContainer: function (el) {
-            return false;
-        },
-
-        // elements are always draggable by default
-        moves: function (el, source, handle, sibling) {
-            return true;
-        },
-
-        // elements can be dropped in any of the `containers` by default
-        accepts: function (el, target, source, sibling) {
-            return true;
-        },
-
-        // don't prevent any drags from initiating by default
-        invalid: function (el, handle) {
-            return false;
-        }
-
-    });
-</script>
+    </body>
 </html>
